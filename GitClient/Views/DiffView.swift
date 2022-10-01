0ab65a45ce961d674392163e873086d5845fe937
Makoto Aoyama <m@aoyama.dev>
2022-10-01 10:07:01 +0900

    Add commit button

--- GitClient/Views/DiffView.swift
+++ GitClient/Views/DiffView.swift
@@ -11,17 +11,103 @@ struct DiffView: View {
     var diff: String
 
     var body: some View {
-        ScrollView {
-            Text(diff)
-                .font(Font.system(.body, design: .monospaced))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
+        ZStack {
+            ScrollView {
+                Text(diff)
+                    .textSelection(.enabled)
+                    .font(Font.system(.body, design: .monospaced))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+            }
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button("Commit") {
+
+                    }
+                    .padding()
+                }
+                .background(.ultraThinMaterial)
+            }
         }
     }
 }
 
 struct DiffView_Previews: PreviewProvider {
     static var previews: some View {
-        DiffView(diff: "hello")
+        DiffView(diff: """
+diff --git a/GitClient/Views/DiffView.swift b/GitClient/Views/DiffView.swift
+index 0cd5c16..114b4ae 100644
+--- a/GitClient/Views/DiffView.swift
++++ b/GitClient/Views/DiffView.swift
+@@ -11,11 +11,25 @@ struct DiffView: View {
+     var diff: String
+
+     var body: some View {
+-        ScrollView {
+-            Text(diff)
+-                .font(Font.system(.body, design: .monospaced))
+-                .frame(maxWidth: .infinity, alignment: .leading)
+-                .padding()
++        ZStack {
++            ScrollView {
++                Text(diff)
++                    .textSelection(.enabled)
++                    .font(Font.system(.body, design: .monospaced))
++                    .frame(maxWidth: .infinity, alignment: .leading)
++                    .padding()
++            }
++            VStack {
++                Spacer()
++                HStack {
++                    Spacer()
++                    Button("Commit") {
++
++                    }
++                    .padding()
++                }
++                .background(.ultraThinMaterial)
++            }
+         }
+     }
+ }
+diff --git a/GitClient/Views/DiffView.swift b/GitClient/Views/DiffView.swift
+index 0cd5c16..114b4ae 100644
+--- a/GitClient/Views/DiffView.swift
++++ b/GitClient/Views/DiffView.swift
+@@ -11,11 +11,25 @@ struct DiffView: View {
+     var diff: String
+
+     var body: some View {
+-        ScrollView {
+-            Text(diff)
+-                .font(Font.system(.body, design: .monospaced))
+-                .frame(maxWidth: .infinity, alignment: .leading)
+-                .padding()
++        ZStack {
++            ScrollView {
++                Text(diff)
++                    .textSelection(.enabled)
++                    .font(Font.system(.body, design: .monospaced))
++                    .frame(maxWidth: .infinity, alignment: .leading)
++                    .padding()
++            }
++            VStack {
++                Spacer()
++                HStack {
++                    Spacer()
++                    Button("Commit") {
++
++                    }
++                    .padding()
++                }
++                .background(.ultraThinMaterial)
++            }
+         }
+     }
+ }
+
+""")
     }
 }

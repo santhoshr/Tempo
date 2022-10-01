7dd077618684c2aedb4809ea5ebc381509b8e89d
Makoto Aoyama <m@aoyama.dev>
2022-10-01 23:19:17 +0900

    Add git group

--- GitClient/Views/DiffView.swift
+++ GitClient/Views/DiffView.swift
@@ -24,7 +24,6 @@ struct DiffView: View {
                 HStack {
                     Spacer()
                     Button("Commit") {
-
                     }
                     .padding()
                 }

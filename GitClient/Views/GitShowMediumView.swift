44dc14bfb83057db33fc5f44a02c450ba3a28fc8
Makoto Aoyama <m@aoyama.dev>
2024-07-14 17:32:45 +0900

    Add text view to GitShowMediumView

--- GitClient/Views/GitShowMediumView.swift
+++ GitClient/Views/GitShowMediumView.swift
@@ -10,11 +10,18 @@ import SwiftUI
 struct GitShowMediumView: View {
     var showMedium: ShowMedium
     var body: some View {
-        Text(showMedium.diff?.raw ?? "")
+        VStack (alignment: .leading) {
+            Text(showMedium.commitHash)
+            Text(showMedium.merge ?? "")
+            Text(showMedium.author)
+            Text(showMedium.date)
+            Text(showMedium.commitMessage)
+            Text(showMedium.diff?.raw ?? "")
+        }
     }
 }
 
-#Preview {
+#Preview("Single diff") {
     let model = try! ShowMedium(raw: """
 commit 4396d158bfa68710f0fef091599e7d1cea310791
 Author: Makoto Aoyama <m@aoyama.dev>
@@ -37,6 +44,22 @@ index ca7d6df..b9d9984 100644
            "version": "5.13.3"
          }
        }
+
+"""
+    )
+    return GitShowMediumView(showMedium: model)
+}
+
+#Preview("Marge commit") {
+    let model = try! ShowMedium(raw: """
+commit 903d5f53d26752e2f87147ae98d326c2f8626e48
+Merge: 850eba7 4351257
+Author: Makoto Aoyama <m@aoyama.dev>
+Date:   Sun Mar 31 12:18:30 2024 +0900
+
+    Merge pull request #1 from maoyama/add-patch
+
+    Add interactive git command
 
 """
     )

d4baf495d4d89ed62ef8f8342a074ad7d88094d5
Makoto Aoyama <m@aoyama.dev>
2024-08-18 11:11:34 +0900

    Add GitSwitchDetach

---
+++ GitClient/Models/Commands/GitSwitchDetach.swift
@@ -0,0 +1,23 @@
+//
+//  GitSwitchDetach.swift
+//  GitClient
+//
+//  Created by Makoto Aoyama on 2024/08/18.
+//
+import Foundation
+
+struct GitSwitchDetach: Git {
+    typealias OutputModel = Void
+    var arguments: [String] {
+        [
+            "git",
+            "switch",
+            "–detach",
+            branchName,
+        ]
+    }
+    var directory: URL
+    var branchName: String
+
+    func parse(for stdOut: String) -> Void {}
+}

2b9665fa59ded614578db3c09a05d1cf7609ab68
Makoto Aoyama <m@aoyama.dev>
2024-09-14 17:46:42 +0900

    Add preview

---
+++ GitClient/Models/Stash.swift
@@ -0,0 +1,13 @@
+//
+//  Stasg.swift
+//  GitClient
+//
+//  Created by Makoto Aoyama on 2024/09/14.
+//
+
+import Foundation
+
+struct Stash: Identifiable {
+    var id: Int
+    var message: String
+}
--- GitClient/Views/StashChangedView.swift
+++ GitClient/Views/StashChangedView.swift
@@ -8,15 +8,27 @@
 import SwiftUI
 
 struct StashChangedView: View {
+    @State var stashList: [Stash] = [
+        .init(id: 0, message: "Hello"),
+        .init(id: 1, message: "World"),
+    ]
+
     var body: some View {
         NavigationSplitView {
- 
+            List(stashList) { stash in
+                Text(stash.message)
+            }
         } detail: {
 
         }
+        .navigationTitle("Stash Changed")
+        .frame(minWidth: 300, minHeight: 200)
     }
 }
 
 #Preview {
-    StashChangedView()
+    StashChangedView(stashList: [
+        .init(id: 0, message: "Hello"),
+        .init(id: 1, message: "World"),
+    ])
 }

091fb8f3fa1c3d164bad042a40be6dd7eba19905
Makoto Aoyama <m@aoyama.dev>
2024-09-19 08:22:17 +0900

    Test Branch.point

---
+++ GitClientTests/BranchTests.swift
@@ -0,0 +1,21 @@
+//
+//  BranchTests.swift
+//  GitClientTests
+//
+//  Created by Makoto Aoyama on 2024/09/19.
+//
+
+import XCTest
+@testable import GitClient
+
+final class BranchTests: XCTestCase {
+    func testPoint() throws {
+        let branch = Branch(name: "main", isCurrent: true)
+        XCTAssertEqual(branch.point, "main")
+    }
+
+    func testPointForDetached() throws {
+        let branch = Branch(name: "(HEAD detached at a036829)", isCurrent: true)
+        XCTAssertEqual(branch.point, "a036829")
+    }
+}

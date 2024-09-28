819eb70523942fe5f1d1173a139cdfce2197af7c
Makoto Aoyama <m@aoyama.dev>
2024-09-28 09:26:19 +0900

    Update settings view

--- GitClient/Views/SettingsView.swift
+++ GitClient/Views/SettingsView.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 
 struct SettingsView: View {
-    
+    @State private var openAIAPISecretKey = ""
+
     var body: some View {
-        Form {
-            Section(header: Text("OpenAI")) {
-                HStack {
-                    Text("Key")
+        VStack(alignment: .leading) {
+            Text("OpenAI API")
+                .font(.title2)
+                .fontWeight(.bold)
+            Divider()
+            Form {
+                Section {
+                    HStack {
+                        SecureField(text: $openAIAPISecretKey) {
+                            Text("SECRET KEY")
+                        }
+                    }
+                } footer: {
+                    Text("""
+    You can generate a commit message from the staged hunk using the OpenAI API.
+
+    Please enter the Secret Key, which can be created from the link https://platform.openai.com/api-keys.
+    Grant “Write” permission to the Secret Key for the “/v1/chat/completions” endpoint.
+    """)
                 }
             }
         }
-        .frame(maxWidth: 600, maxHeight: 500)
+        .padding()
+        .frame(minWidth: 300, maxWidth: 700, minHeight: 200, maxHeight: 600)
     }
 }
 

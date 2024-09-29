b3e031249117f2f3b3a7030b17d468d46aa74e00
Makoto Aoyama <m@aoyama.dev>
2024-09-29 13:10:24 +0900

    Add privacy information regarding inputs and outputs usage to SettingsView

--- GitClient/Views/SettingsView.swift
+++ GitClient/Views/SettingsView.swift
@@ -31,6 +31,8 @@ Please enter the Secret Key, which can be created from the link https://platform
 Grant “Write” permission to the Secret Key for the “/v1/chat/completions” endpoint.
 
 Each time the Generate button is clicked, a request for changes will be sent to the API. You can check the costs associated with using the API here. https://platform.openai.com/usage
+
+This Git client app and OpenAI API also do not use the inputs and outputs for model training. https://openai.com/enterprise-privacy
 """
                     )
                 }

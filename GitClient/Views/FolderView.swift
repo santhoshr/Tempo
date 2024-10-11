4aaf26216b6ac5033bca830228a5cb9c2f951c80
Makoto Aoyama <m@aoyama.dev>
2024-10-12 00:25:54 +0900

    Revert "Add icon for log display"
    This reverts commit 32040cc8846d8691f8fa72da2c761a1d4b705073.

--- GitClient/Views/FolderView.swift
+++ GitClient/Views/FolderView.swift
@@ -217,32 +217,22 @@ struct FolderView: View {
         }
     }
 
-    fileprivate func logsRow(_ log: Log) -> some View {
+    fileprivate func logsRow(_ log: Log) -> VStack<_ConditionalContent<Text, some View>> {
         return VStack {
             switch log {
             case .notCommitted:
-                HStack {
-                    Image(systemName: "plus.forwardslash.minus")
-                        .frame(width: 12)
-                        .foregroundStyle(.tertiary)
-                    Text("Not Committed")
-                        .foregroundStyle(.secondary)
-                }
+                Text("Not Committed")
+                    .foregroundStyle(Color.secondary)
             case .committed(let commit):
-                HStack {
-                    Image(systemName: commit.abbreviatedParentHashes.count == 2 ? "asterisk.circle.fill" : "asterisk")
-                        .foregroundStyle(.tertiary)
-                        .frame(width: 12)
-                    VStack (alignment: .leading) {
-                        Text(commit.title)
-                        HStack {
-                            Text(commit.author)
-                            Spacer()
-                            Text(commit.authorDateRelative)
-                        }
-                        .lineLimit(1)
-                        .foregroundStyle(.tertiary)
+                VStack (alignment: .leading) {
+                    Text(commit.title)
+                    HStack {
+                        Text(commit.author)
+                        Spacer()
+                        Text(commit.authorDateRelative)
                     }
+                    .lineLimit(1)
+                    .foregroundStyle(.tertiary)
                 }
                 .contextMenu {
                     Button("Checkout") {

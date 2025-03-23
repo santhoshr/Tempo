5785f89449a5184c544b98570847041b14050362
Makoto Aoyama <m@aoyama.dev>
2025-03-23 15:55:56 +0900

    Fix ContentView ID computation to include commit tags for better uniqueness.

--- GitClient/Views/ContentView.swift
+++ GitClient/Views/ContentView.swift
@@ -101,7 +101,7 @@ struct ContentView: View {
                 )
             case .committed(let commit):
                 CommitDetailStackView(commitHash: commit.hash, folder: selectionFolder!)
-                    .id(commit.hash + commit.branches.joined())
+                    .id(commit.hash + commit.branches.joined() + commit.tags.joined())
             case nil:
                 Text("No Selection")
                     .foregroundColor(.secondary)

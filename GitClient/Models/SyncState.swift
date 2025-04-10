3d0df2c45982fabe7f4572ff95040058daf6b806
Makoto Aoyama <m@aoyama.dev>
2025-04-10 15:41:16 +0900

    Add syncing functionality to SyncState model with methods for pull and push checks

--- GitClient/Models/SyncState.swift
+++ GitClient/Models/SyncState.swift
@@ -10,6 +10,18 @@ import Observation
 
 @MainActor
 @Observable class SyncState {
-    
+    var folder: Folder?
+    var branch: Branch?
+    var shouldPull = false
+    var shouldPush = false
 
+    func sync() async throws {
+        guard let folder, let branch else {
+            shouldPull = false
+            shouldPush = false
+            return
+        }
+        try await Process.output(GitFetch(directory: folder.url))
+        shouldPull = !(try await Process.output(GitLog(directory: folder.url, revisionRange: "\(branch.name)..origin/\(branch.name)" )).isEmpty)
+    }
 }

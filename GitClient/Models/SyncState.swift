25adcfd6dca3f3893407f0c67b72f78564b09a91
Makoto Aoyama <m@aoyama.dev>
2025-04-10 13:45:45 +0900

    Add SyncState

---
+++ GitClient/Models/SyncState.swift
@@ -0,0 +1,15 @@
+//
+//  SyncState.swift
+//  GitClient
+//
+//  Created by Makoto Aoyama on 2025/04/10.
+//
+
+import Foundation
+import Observation
+
+@MainActor
+@Observable class SyncState {
+    
+
+}

382422f816b6b252a9c9307b1425f29145a3dee0
Makoto Aoyama <m@aoyama.dev>
2025-04-12 09:54:31 +0900

    Add support for 'revisionRange' to SearchKind and update related filtering logic in LogStore and SearchTokensHandler.

--- GitClient/Models/Observables/LogStore.swift
+++ GitClient/Models/Observables/LogStore.swift
@@ -17,7 +17,7 @@ import Observation
             switch token.kind {
             case .grep, .grepAllMatch:
                 return true
-            case .g, .s, .author:
+            case .g, .s, .author, .revisionRange:
                 return false
             }
         }.map { $0.text }

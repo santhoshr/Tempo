a29269e485473250b0b7713d7bd12efd36087ab5
Makoto Aoyama <m@aoyama.dev>
2025-04-24 08:38:34 +0900

    Add GitRevListCount command to retrieve the count of Git revisions.
    - Introduced a new struct `GitRevListCount` to encapsulate functionality for counting commits.
    - Updated project file to include the new source file.

---
+++ GitClient/Models/Commands/GitRevListCount.swift
@@ -0,0 +1,27 @@
+//
+//  GitRevList.swift
+//  GitClient
+//
+//  Created by Makoto Aoyama on 2025/04/24.
+//
+
+import Foundation
+
+struct GitRevListCount: Git {
+    typealias OutputModel = Int?
+    var arguments: [String] {
+        var args = [
+            "git",
+            "rev-list",
+            "--count"
+        ]
+        args.append(commit)
+        return args
+    }
+    var directory: URL
+    var commit = "HEAD"
+
+    func parse(for stdOut: String) -> Int? {
+        Int(stdOut)
+    }
+}

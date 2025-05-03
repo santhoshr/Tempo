1d9b4f6ae816b3207589d297209b255044ca74a8
Makoto Aoyama <m@aoyama.dev>
2025-05-03 11:37:31 +0900

    Fix the navigation stack view updating on graph view

--- GitClient/Views/Commit/CommitDetailStackView.swift
+++ GitClient/Views/Commit/CommitDetailStackView.swift
@@ -38,6 +38,8 @@ struct CommitDetailStackView: View {
                 }
                 .navigationBarBackButtonHidden()
         }
-
+        .onChange(of: commit) { oldValue, newValue in
+            path = []
+        }
     }
 }

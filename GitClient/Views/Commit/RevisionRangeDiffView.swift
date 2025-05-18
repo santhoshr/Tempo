8215f2a94b503050ce69c0c79650236c321b8836
Makoto Aoyama <m@aoyama.dev>
2025-05-19 08:45:06 +0900

    Add error handling and Update diff for input text
    - Introduced error state to manage and display errors during git operations.
    - Wrapped async operations in do-catch blocks to handle potential errors when fetching commits and diffs.
    - Integrated errorSheet to present errors to the user.

--- GitClient/Views/Commit/RevisionRangeDiffView.swift
+++ GitClient/Views/Commit/RevisionRangeDiffView.swift
@@ -14,6 +14,7 @@ struct RevisionRangeDiffView: View {
     @State private var commits: [Commit] = []
     @State private var filesChanges: [ExpandableModel<FileDiff>] = []
     @State private var revisionRangeText = ""
+    @State private var error: Error?
 
     var body: some View {
         ScrollView {
@@ -33,7 +34,18 @@ struct RevisionRangeDiffView: View {
                             .textFieldStyle(.roundedBorder)
                             .frame(width: 160)
                             .onSubmit {
-                                print("Submit" + revisionRangeText)
+                                guard let folder else { return }
+                                Task {
+                                    do {
+                                        commits = try await Array(Process.output(GitLog(directory: folder, revisionRange: revisionRangeText)))
+                                        let raw = try await Process.output(
+                                            GitDiff(directory: folder, noRenames: false, revisionRange: revisionRangeText)
+                                        )
+                                        filesChanges = try Diff(raw: raw).fileDiffs.map { .init(isExpanded: true, model: $0) }
+                                    } catch {
+                                        self.error = error
+                                    }
+                                }
                             }
                     }
                     Spacer()
@@ -44,15 +56,20 @@ struct RevisionRangeDiffView: View {
             .onChange(of: selectionLogID + subSelectionLogID, initial: true) { oldValue, newValue in
                 Task {
                     guard let folder else { return }
-                    revisionRangeText = rangeText(logID: subSelectionLogID).prefix(8) + "..." + rangeText(logID: selectionLogID).prefix(8)
-                    let revisionRange = "\(rangeText(logID: subSelectionLogID))...\(rangeText(logID: selectionLogID))"
-                    commits = try await Array(Process.output(GitLog(directory: folder, revisionRange: revisionRange)))
-                    let raw = try await Process.output(
-                        GitDiff(directory: folder, noRenames: false, revisionRange: revisionRange)
-                    )
-                    filesChanges = try Diff(raw: raw).fileDiffs.map { .init(isExpanded: true, model: $0) }
+                    do {
+                        revisionRangeText = rangeText(logID: subSelectionLogID).prefix(8) + "..." + rangeText(logID: selectionLogID).prefix(8)
+                        let revisionRange = "\(rangeText(logID: subSelectionLogID))...\(rangeText(logID: selectionLogID))"
+                        commits = try await Array(Process.output(GitLog(directory: folder, revisionRange: revisionRange)))
+                        let raw = try await Process.output(
+                            GitDiff(directory: folder, noRenames: false, revisionRange: revisionRange)
+                        )
+                        filesChanges = try Diff(raw: raw).fileDiffs.map { .init(isExpanded: true, model: $0) }
+                    } catch {
+                        self.error = error
+                    }
                 }
             }
+            .errorSheet($error)
     }
 
     private func rangeText(logID: String) -> String {

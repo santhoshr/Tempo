f99ad38aad7012fe7ebdaf09c031ed5776655aa0
Makoto Aoyama <m@aoyama.dev>
2025-05-25 19:10:36 +0900

    Add AuthorInitialIcon view for displaying user initials
    - Introduced AuthorInitialIcon.swift to render a circular gradient icon with user initials.
    - Updated project.pbxproj to include new view in the Xcode project.

---
+++ GitClient/Views/Folder/AuthorInitialIcon.swift
@@ -0,0 +1,42 @@
+//
+//  AuthorInitialIcon.swift
+//  GitClient
+//
+//  Created by Makoto Aoyama on 2025/05/25.
+//
+
+import SwiftUI
+
+struct AuthorInitialIcon: View {
+    var initial: String
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 0, style: .continuous)
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(colors: [Color(white: 0.65), Color(white: 0.50)]),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+            Text(initial)
+                .fontDesign(.rounded)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+#Preview {
+    AuthorInitialIcon(initial: "A")
+        .frame(width: 40, height: 40)
+}
+
+#Preview {
+    AuthorInitialIcon(initial: "AA")
+        .frame(width: 40, height: 40)
+}
+
+#Preview {
+    AuthorInitialIcon(initial: "AAAAAAAAA")
+        .frame(width: 40, height: 40)
+}

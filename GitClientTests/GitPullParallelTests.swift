8f8b043572e6f5485c406c224f9e628492e7b9a6
Makoto Aoyama <m@aoyama.dev>
2025-05-27 09:03:35 +0900

    Add tests file

---
+++ GitClientTests/GitPullParallelTests.swift
@@ -0,0 +1,16 @@
+//
+//  GitPullParallelTests.swift
+//  GitClientTests
+//
+//  Created by Makoto Aoyama on 2025/05/27.
+//
+
+import Testing
+
+struct GitPullParallelTests {
+
+    @Test func gitPull() async throws {
+        
+    }
+
+}

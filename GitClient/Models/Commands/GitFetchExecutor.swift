85feeef0e1d9c13ce5fec554e2469c605df9efe2
Makoto Aoyama <m@aoyama.dev>
2025-05-27 22:14:25 +0900

    Remove debug print statements from GitFetchExecutor commands.

--- GitClient/Models/Commands/GitFetchExecutor.swift
+++ GitClient/Models/Commands/GitFetchExecutor.swift
@@ -11,14 +11,10 @@ actor GitFetchExecutor {
     static let shared = GitFetchExecutor()
 
     func execute(_ command: GitFetch) throws {
-        print("start git fetch")
         try Process.outputSync(command)
-        print("end git fetch")
     }
 
     func execute(_ command: GitPull) throws {
-        print("start git pull")
         try Process.outputSync(command)
-        print("end git pull")
     }
 }

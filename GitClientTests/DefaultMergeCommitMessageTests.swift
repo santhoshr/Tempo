11718b0a53dd9077286b56ec0dcf31338c082b9b
Makoto Aoyama <m@aoyama.dev>
2025-06-14 09:33:22 +0900

    Add DefaultMergeCommitMessageTests to GitClientTests
    - Included new test for default merge commit message generation.
    - Updated project.pbxproj to reference the new test file.
    - Updated subproject commit in SyntaxHighlight.

---
+++ GitClientTests/DefaultMergeCommitMessageTests.swift
@@ -0,0 +1,37 @@
+//
+//  DefaultMergeCommitMessageTests.swift
+//  GitClientTests
+//
+//  Created by Makoto Aoyama on 2025/06/14.
+//
+
+import Testing
+@testable import Tempo
+import Foundation
+
+struct DefaultMergeCommitMessageTests {
+
+    @Test func get() async throws {
+        try await Process.output(GitSwitch(directory: .testFixture!, branchName: "_test-fixture-conflict2"))
+        do {
+            try await Process.output(GitMerge(directory: .testFixture!, branchName: "_test-fixture-conflict"))
+        } catch {
+            // A conflict error occurred
+        }
+        let string = try await DefaultMergeCommitMessage(directory: .testFixture!).get()
+        let expectedString = """
+Merge branch '_test-fixture-conflict' into _test-fixture-conflict2
+
+# Conflicts:
+#\tExamples/Examples/ContentView.swift
+#\tREADME.md
+"""
+
+        #expect(string == expectedString)
+
+        //　Cleanup
+        try await Process.output(GitAdd(directory: .testFixture!))
+        try await Process.output(GitStash(directory: .testFixture!))
+    }
+
+}

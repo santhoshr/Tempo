b731b65d4eafae6677bd5462881d38c525e9faa4
Makoto Aoyama <m@aoyama.dev>
2025-06-17 21:18:40 +0900

    Add GeneratedCommitMessage struct for improved commit message generation in SystemLanguageModelService

--- GitClient/Models/AIService/SystemLanguageModelService.swift
+++ GitClient/Models/AIService/SystemLanguageModelService.swift
@@ -9,10 +9,17 @@ import Foundation
 import FoundationModels
 
 @available(macOS 26.0, *)
+@Generable
+struct GeneratedCommitMessage {
+    @Guide(description: "The commit message")
+    var commitMessage: String
+}
+
+@available(macOS 26.0, *)
 struct SystemLanguageModelService {
     func commitMessage(stagedDiff: String) async throws -> String {
         let instructions = "You are a good software engineer. I'll provide the result of git diff as a prompt, so please return only the commit message."
         let session = LanguageModelSession(instructions: instructions)
-        return try await session.respond(to: stagedDiff).content
+        return try await session.respond(to: stagedDiff, generating: GeneratedCommitMessage.self).content.commitMessage
     }
 }

e5b264322c4bb0e37c1f8f9d93132e3ac961e618
Makoto Aoyama <m@aoyama.dev>
2025-06-17 21:09:04 +0900

    Update test

--- GitClientTests/SystemLanguageModelServiceTests.swift
+++ GitClientTests/SystemLanguageModelServiceTests.swift
@@ -30,19 +30,35 @@ struct SystemLanguageModelServiceTests {
         #expect(!message.isEmpty)
 
         let message2 = try await SystemLanguageModelService().commitMessage(stagedDiff: """
-            diff --git a/GitClient/Views/ContentView.swift b/GitClient/Views/ContentView.swift
-            index 3ea91cd..2c5dd6f 100644
-            --- a/GitClient/Views/ContentView.swift
-            +++ b/GitClient/Views/ContentView.swift
-            @@ -33,7 +33,7 @@ struct ContentView: View {
-                             if decodedFolders.isEmpty {
-                                 VStack {
-                                     Text("No Project Folder Added")
-            -                        Text("Please add a folder that contains a Git repository")
-            +                        Text("Please add a folder that contains a Git repository.")
-                                         .font(.caption)
-                                         .padding(.top, 2)
-                                 }
+            diff --git a/GitClient/Models/Observables/LogStore.swift b/GitClient/Models/Observables/LogStore.swift
+            index 8a43562..226c84e 100644
+            --- a/GitClient/Models/Observables/LogStore.swift
+            +++ b/GitClient/Models/Observables/LogStore.swift
+            @@ -147,6 +147,24 @@ import Observation
+                     }
+                 }
+             
+            +    func nextLogID(logID: String) -> String? {
+            +        if logID == Log.notCommitted.id {
+            +            return commits.first?.id
+            +        }
+            +        let commit = commits.first { $0.id == logID }
+            +        guard let commit else { return nil }
+            +        return commit.parentHashes.last
+            +    }
+            +
+            +    func previousLogID(logID: String) -> String? {
+            +        if logID == Log.notCommitted.id {
+            +            return nil
+            +        }
+            +        let index = commits.firstIndex { $0.id == logID }
+            +        guard let index, index != 0 else { return nil }
+            +        return commits[index - 1].id
+            +    }
+            +
+                 private func notCommited(directory: URL) async throws -> NotCommitted {
+                     let gitDiff = try await Process.output(GitDiff(directory: directory))
+                     let gitDiffCached = try await Process.output(GitDiffCached(directory: directory))
             """)
         print(message2)
         #expect(!message2.isEmpty)

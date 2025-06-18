c31d6b06e0c4dd87499c381017dc8d85428587fc
Makoto Aoyama <m@aoyama.dev>
2025-06-18 21:07:43 +0900

    Updated instructions for crafting effective commit messages

--- GitClient/Models/AIService/SystemLanguageModelService.swift
+++ GitClient/Models/AIService/SystemLanguageModelService.swift
@@ -18,7 +18,20 @@ struct GeneratedCommitMessage {
 @available(macOS 26.0, *)
 struct SystemLanguageModelService {
     func commitMessage(stagedDiff: String) async throws -> String {
-        let instructions = "You are a good software engineer. When creating a commit message, it is not the initial commit."
+        let instructions = """
+You are a good software engineer. When creating a commit message, it is not the initial commit. 
+The output format of git diff is as follows:
+```
+diff --git a/filename b/filename
+index abc1234..def5678 100644
+--- a/filename
++++ b/filename
+@@ -start,count +start,count @@ optional context or function name
+- line that was removed
++ line that was added
+  unchanged line (context)
+```
+"""
         let prompt = "Please provide an appropriate commit message for the following changes: \(stagedDiff)"
         let session = LanguageModelSession(instructions: instructions)
         return try await session.respond(to: prompt, generating: GeneratedCommitMessage.self).content.commitMessage

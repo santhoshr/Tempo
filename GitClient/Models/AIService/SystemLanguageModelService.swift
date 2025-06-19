703d742453d7d572c0e85a7d6e90c83aff6d3959
Makoto Aoyama <m@aoyama.dev>
2025-06-19 20:59:36 +0900

    Added UncommitedChangesTool

--- GitClient/Models/AIService/SystemLanguageModelService.swift
+++ GitClient/Models/AIService/SystemLanguageModelService.swift
@@ -36,4 +36,32 @@ index abc1234..def5678 100644
         let session = LanguageModelSession(instructions: instructions)
         return try await session.respond(to: prompt, generating: GeneratedCommitMessage.self).content.commitMessage
     }
+    
+    func commitMessage(tools: [any Tool]) async throws -> String {
+        let instructions = """
+You are a good software engineer. When creating a commit message, it is not the initial commit.
+"""
+        let prompt = "Please provide an appropriate commit message for staged changes of uncommitted changes"
+        let session = LanguageModelSession(tools: tools, instructions: instructions)
+        return try await session.respond(to: prompt, generating: GeneratedCommitMessage.self).content.commitMessage
+    }
+
+}
+
+@available(macOS 26.0, *)
+struct UncommitedChangesTool: Tool {
+    @Generable
+    struct Arguments {}
+    
+    let name = "uncommitedChanges"
+    let description: String = "Get a uncommitted changes"
+    let directory: URL
+    
+    func call(arguments: Arguments) async throws -> ToolOutput {
+        let gitDiff = try await Process.output(GitDiff(directory: directory))
+        let gitDiffCached = try await Process.output(GitDiffCached(directory: directory))
+        let diff = try Diff(raw: gitDiff).fileDiffs.map { $0.raw }
+        let cachedDiff = try Diff(raw: gitDiffCached).fileDiffs.map { $0.raw }
+        return ToolOutput(GeneratedContent(properties: ["stagedChanges": cachedDiff, "unstagedChanges": diff]))
+    }
 }

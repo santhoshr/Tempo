435be0d855d7431dbd9725736cae2fff1af62026
Makoto Aoyama <m@aoyama.dev>
2025-06-21 19:30:35 +0900

    Add buttons to expand and collapse all file diffs in FileDiffsView
    - Implemented 'Expand All Files' and 'Collapse All Files' functionality
    - Added button styles and help text for better user guidance
    - Adjusted padding for improved layout clarity

--- GitClient/Views/Commit/FileDiffsView.swift
+++ GitClient/Views/Commit/FileDiffsView.swift
@@ -12,6 +12,23 @@ struct FileDiffsView: View {
 
     var body: some View {
         LazyVStack(alignment: .leading, spacing: 0) {
+            HStack {
+                Spacer()
+                Button {
+                    expandableFileDiffs = expandableFileDiffs.map { ExpandableModel(isExpanded: true, model: $0.model)}
+                } label: {
+                    Image(systemName: "arrow.up.and.line.horizontal.and.arrow.down")
+                }
+                .help("Expand All Files")
+                Button {
+                    expandableFileDiffs = expandableFileDiffs.map { ExpandableModel(isExpanded: false, model: $0.model)}
+                } label: {
+                    Image(systemName: "arrow.down.and.line.horizontal.and.arrow.up")
+                }
+                .help("Collapse All Files")
+            }
+            .buttonStyle(.accessoryBar)
+
             ForEach($expandableFileDiffs, id: \.self) { $expandedFileDiff in
                 FileDiffView(
                     expandableFileDiff: $expandedFileDiff,
@@ -20,8 +37,9 @@ struct FileDiffsView: View {
                     }
                 )
             }
+            .padding(.top, 2)
         }
         .font(Font.system(.body, design: .monospaced))
-        .padding(.top)
+        .padding(.top, 6)
     }
 }

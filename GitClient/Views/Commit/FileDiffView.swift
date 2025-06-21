26e31e3bcd57c4d3690563c693dd862404a1bf68
Makoto Aoyama <m@aoyama.dev>
2025-06-21 18:41:23 +0900

    Enhance FileDiffView and FileDiffsView layout with padding adjustments
    - Increased top and bottom padding in chunks view for better spacing in FileDiffView.
    - Removed vertical padding in FileDiffsView and applied only top padding for improved layout.

--- GitClient/Views/Commit/FileDiffView.swift
+++ GitClient/Views/Commit/FileDiffView.swift
@@ -15,6 +15,8 @@ struct FileDiffView: View {
         DisclosureGroup(isExpanded: $expandableFileDiff.isExpanded) {
             LazyVStack(alignment: .leading, spacing: 0, pinnedViews: .sectionHeaders) {
                     chunksView(expandableFileDiff.model.chunks, filePath: expandableFileDiff.model.toFilePath)
+                    .padding(.top, 8)
+                    .padding(.bottom, 12)
             }
         } label: {
             FileNameView(toFilePath: expandableFileDiff.model.toFilePath, filePathDisplay: expandableFileDiff.model.filePathDisplay)
@@ -25,7 +27,6 @@ struct FileDiffView: View {
     private func chunksView(_ chunks: [Chunk], filePath: String) -> some View {
         ForEach(chunks) { chunk in
             ChunkView(chunk: chunk, filePath: filePath)
-                .padding(.bottom)
         }
     }
 }

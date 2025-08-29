3e90fa9d6574df9188fba13b44a1ac77172dc279
Santhosh R <santhoshr@live.com>
2025-08-29 17:43:14 +0530

    Chore: gitignore update and disable excess git logging when debugging

--- GitClient/Extensions/Process+Run.swift
+++ GitClient/Extensions/Process+Run.swift
@@ -39,9 +39,9 @@ extension Process {
 
     static private func outputSync(arguments: [String], currentDirectoryURL: URL?, inputs: [String]=[]) throws -> Output {
         let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "process")
-        logger.debug("Process run: arguments: \(arguments), currentDirectoryURL: \(currentDirectoryURL?.description ?? ""), inputs: \(inputs, privacy: .public)")
+        // logger.debug("Process run: arguments: \(arguments), currentDirectoryURL: \(currentDirectoryURL?.description ?? ""), inputs: \(inputs, privacy: .public)")
         let output = try run(arguments: arguments, currentDirectoryURL: currentDirectoryURL, inputs: inputs)
-        logger.debug("Process output of \(arguments): \(output.standardOutput + output.standartError, privacy: .public)")
+        // logger.debug("Process output of \(arguments): \(output.standardOutput + output.standartError, privacy: .public)")
         return output
     }
 
